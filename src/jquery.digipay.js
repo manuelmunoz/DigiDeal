acb875aefec0cd440e8475239df7ccb25466a1be
BeatnutNL <37551042+BeatnutNL@users.noreply.github.com>
2018-03-26 10:52:28 +0200

    Add files via upload

--- src/jquery.digipay.js
+++ src/jquery.digipay.js
@@ -1,108 +1,131 @@
 
 (function($) {
 	
-	var DGB = new DGBO();
+
 	var statusObject = {};
 	var txcount = 0;
 	var pvk;
 	var unit = digibyte.Unit;
 		
-	function generatePaymentRequest(settings) {
-	
-		// check if a session is already in the address
-		var data= fgp('data') || settings.data;
-
-		var ppk = fgp('ppk');
+	class DigiPay {
 		
 		
-		var qrimage = $('<img src="img/qr.png"/>');
-		
-		
-		
-		pvk = (ppk ? digibyte.PrivateKey.fromWIF(atob(ppk)) : new digibyte.PrivateKey());
-
-		// create pvk WIFF address from the pvk.
-		var bufferPrivateKey = pvk.toWIF().toString();
-		var bufferPublicAddress = pvk.publicKey.toAddress().toString();
-		
-		// make savestate for if someone accidentaly presses F5
-		history.pushState('', '', '?ppk='+btoa(bufferPrivateKey)+'&data='+data);
-		
-		
-		/* Generate HT
-		L */
-		// main element
-		var main = $('<div class="digiwrapper"><div style="height:'+(settings.size+40)+'px;" class="statusimage"></div> </div>'); 
-		
-		
-
-		// add qr
-		var qramount = unit.fromSatoshis(settings.amount+settings.fee).toBTC();
-		
-		
-		qrimage.on('load',function() {
-			createQr({
-				'text':"digibyte:"+bufferPublicAddress+'?amount='+qramount,
-				'size':settings.size,
-	
-			})
+		constructor(settings,main) {
+			this.settings = settings;
+			this.statusObject = {};
+			this.data = this.fgp('data') || settings.data;
+			var ppk = this.fgp('ppk');
+			this.pvk = (ppk ? digibyte.PrivateKey.fromWIF(atob(ppk)) : new digibyte.PrivateKey());
+			this.DGB = new DGBO();
+			this.txcount = 0;
+			this.unit = digibyte.Unit;
+			this.bufferPrivateKey = this.pvk.toWIF().toString();
+			this.bufferPublicAddress = this.pvk.publicKey.toAddress().toString();
+			history.pushState('', '', '?ppk='+btoa(this.bufferPrivateKey)+'&data='+this.data);
+			this.qrimage = $('<img src="img/qr.png"/>');
+			this.main = $('<div class="digiwrapper"></div>');
 			
-		});
-		
-
-		
-		
-		// add details of transaction
-				
-		var details = $('<div class="details"></div>');
-		
-		details.append('<div class="amount">Remaining '+unit.fromSatoshis(settings.amount).toBTC()+' DGB + ('+unit.fromSatoshis(settings.fee).toBTC()+'Fee)</div>');
-		details.append('<div class="address">'+bufferPublicAddress+'</div>');
-		details.append('<div class="txid">Txid:'+data+'</div>');
-		main.append(details);
-		
-		// add status			
-
-		main.append(
-			createStatusbars()
-		);		
-
-		
-		
-		// check loop for payment check;
-		checkPayment(bufferPublicAddress);
-		
-		if(settings.onInitialize) {
-			// return a callback depending if it is set.
-			settings.onInitialize({
-				publicKey:bufferPublicAddress,
-				privateKey:bufferPrivateKey,
-			});
+			
+			
+			
+			
+			
 		}
 		
-		// Return the html element to be inserted using jquery
 		
-		return main;
-	
+		
+		getHtml() {
+			// check if a session is already in the address
+		
 
+			
+			
+			
+			
+			
+			
+			
+			// create pvk WIFF address from the pvk.
+
+			
+			// make savestate for if someone accidentaly presses F5
+			
+			
+			/* Generate HT
+			L */
+			// main element
+			this.main.append('<div style="height:'+(this.settings.size+40)+'px;" class="statusimage"></div> </div>'); 
+			
+			
+
+			// add qr
+			var qramount = this.unit.fromSatoshis(this.settings.amount+this.settings.fee).toBTC();
+			
+			
+			this.qrimage.on('load',()=> {
+				this.createQr({
+					'text':"digibyte:"+this.bufferPublicAddress+'?amount='+qramount,
+					'size':this.settings.size,
+		
+				})
+				
+			});
+			
+
+			
+			
+			// add details of transaction
+					
+			var details = $('<div class="details"></div>');
+			
+			details.append('<div class="amount">To pay '+this.unit.fromSatoshis(this.settings.amount).toBTC()+' DGB + ('+this.unit.fromSatoshis(this.settings.fee).toBTC()+'Fee)</div>');
+			details.append('<div class="address">'+this.bufferPublicAddress+'</div>');
+			details.append('<div class="txid">Txid:'+this.data+'</div>');
+			
+			this.main.append(details);
+			
+			// add status			
+
+			this.main.append(
+				this.createStatusbars()
+			);		
+
+			
+			
+			// check loop for payment check;
+			this.checkPayment();
+			
+			if(this.settings.onInitialize) {
+				// return a callback depending if it is set.
+				this.settings.onInitialize({
+					publicKey:this.bufferPublicAddress,
+					privateKey:this.bufferPrivateKey,
+				});
+			}
+			
+			// Return the html element to be inserted using jquery
+			
+			return this.main;
+		
+		}
 		
 	
 		
 		
 		
-		function createQr(qs) {
+		createQr(qs) {
 				// generate QR with qs settings
 				
-				qs.image = qrimage[0];
+				qs.image = this.qrimage[0];
 				qs.mode = 4
 				qs.mSize = 0.30;
 				qs.ecLevel = 'Q'
-				return main.children('.statusimage').html($('<div class="qr"></div>').qrcode(qs).fadeIn('slow'));
+				return this.main.children('.statusimage').html($('<div class="qr"></div>').qrcode(qs).fadeIn('slow'));
 
 
 		}
 		
-		function createStatusbars() {
+		createStatusbars() {
 			
 			// create 2 status bars that update according to stage in transaction
 			var astatus = $('<div class="status"></div>');			
@@ -118,19 +141,21 @@
 			return astatus;
 
 		}
-		function getStatus() {
-			return statusObject;
+		
+		
+		getStatus() {
+			return this.statusObject;
 				
 		}
 			
-		function setStatus(name,value) {
+		setStatus(name,value) {
 			
-			statusObject[name] = value;
+			this.statusObject[name] = value;
 		}
 			
-		function checkPayment(address) {
-			getTransactions(address).then((txids)=>{
-				return checkTransactions(txids)}).then(
+		checkPayment() {
+			this.getTransactions(this.bufferPublicAddress).then((txids)=>{
+				return this.checkTransactions(txids)}).then(
 					results=>{
 					txcount = results.length;
 					
@@ -143,7 +168,7 @@
 					}
 					results.sort(byBlocktime);
 					
-					DGB.getWalletValue(bufferPublicAddress).then(walletval=>{
+					this.DGB.getWalletValue(this.bufferPublicAddress).then(walletval=>{
 						
 						// convert the balance to satoshis.
 						var trec = walletval.totalReceivedSat;
@@ -151,56 +176,55 @@
 
 						// check if the balance matches the specified amount;
 						
-						if(trec >= settings.amount+settings.fee) {
+						if(trec >= this.settings.amount+this.settings.fee) {
 							
 							// send the total wallet of the bufferaddress to the specified address;
 							if(balance > 0) {
-								finishPayment(results[0].vin[0].addr).then(result=>{
-									setStatus('txs',results);
-									if(trec > settings.amount+settings.fee) {
+								this.finishPayment(results[0].vin[0].addr).then(result=>{
+									this.setStatus('txs',results);
+									if(trec > this.settings.amount+this.settings.fee) {
 										var message = 'Payment overpaid, sending the overspend back to last used public key';
 									} else {
 										var message = 'Payment completed';
 									}
-									showStatus('transactions',message,'ready');
-									success('Payment done');
+									this.showStatus('transactions',message,'ready');
+									this.success('Payment done');
 								});
 							} else {
-								showStatus('transactions','Payment already paid','ready');
-								setStatus('txs',results);
+								this.showStatus('transactions','Payment already paid','ready');
+								this.setStatus('txs',results);
 								// last tx is endtx. todo.
-								setStatus('endTx',results[results.length-1]);
-								success('Payment completed previously');
+								this.setStatus('endTx',results[results.length-1]);
+								this.success('Payment completed previously');
 							}
 						
 							
 						} else {
 							// paid not enough
-							var needed = settings.amount+settings.fee-trec;
-								showStatus('transactions','Transaction underpaid by '+(needed),'pending');
+							var needed = this.settings.amount+this.settings.fee-trec;
+								this.showStatus('transactions','Transaction underpaid by '+(needed),'pending');
 							// first remake a new payment;	
 								
 								// recreate the QR
-								createQr(	{'text':"digibyte:"+bufferPublicAddress+'?amount='+unit.fromSatoshis((needed)).toBTC(),
-									'size':settings.size
+								this.createQr(	{'text':"digibyte:"+this.bufferPublicAddress+'?amount='+this.unit.fromSatoshis((needed)).toBTC(),
+									'size':this.settings.size
 									}
 								)
 								// reupdate the amount;
-								$(main).find('.amount').html('Remaining '+unit.fromSatoshis((needed)).toBTC()+' DGB');
-								
+
 								// recheck the address for the amount
-								checkPayment(bufferPublicAddress);	
+								this.checkPayment();	
 						}
 						
 					},
 					error=>{
-						fail(error)}
+						this.fail(error)}
 					);
 
 				},
 				error=>{
-					console.log(error);
-					fail(error);
+					
+					this.fail(error);
 					
 				}
 			);
@@ -208,51 +232,51 @@
 
 		}
 		
-		function fail(message) {
-			setStatus('message',message);
-			setStatus('confirmed',false);
-			showStatus('address',message,'failed');
-			if(settings.onFail) {
-				settings.onFail(getStatus());
+		fail(message) {
+			this.setStatus('message',message);
+			this.setStatus('confirmed',false);
+			this.showStatus('address',message,'failed');
+			if(this.settings.onFail) {
+				this.settings.onFail(this.getStatus());
 			}
 			
 		}
 		
-		function success(message) {
-			setStatus('message',message);
-			setStatus('confirmed',true);
+		success(message) {
+			this.setStatus('message',message);
+			this.setStatus('confirmed',true);
 			// show the status as ready and payment done
 			// replace the QR with a checkmark.
-			var checkimg = $('<img class="check" height="'+settings.size+'" width="'+settings.size+'" src="img/check.svg"/>').hide();
-			main.find('.statusimage').html(checkimg);
+			var checkimg = $('<img class="check" height="'+this.settings.size+'" width="'+this.settings.size+'" src="img/check.svg"/>').hide();
+			this.main.find('.statusimage').html(checkimg);
 			checkimg.fadeIn('slow');
 			// if there is a callback for succes, execute it.
-			if(settings.onSuccess) {
-				settings.onSuccess(getStatus());
+			if(this.settings.onSuccess) {
+				this.settings.onSuccess(this.getStatus());
 			}
 			
 			
 		}
 		
-		function showLoader() {
+		showLoader() {
 			
-			if(main.find('.loader').length == 0) {
-				var loader = $('<div style="width:'+settings.size+'px;height:'+settings.size+'px;" class="loader"></div>').hide();
-				$(main).find('.statusimage').html(loader);
+			if(this.main.find('.loader').length == 0) {
+				var loader = $('<div style="width:'+this.settings.size+'px;height:'+this.settings.size+'px;" class="loader"></div>').hide();
+				this.main.find('.statusimage').html(loader);
 				loader.fadeIn('slow');
 			}
 		}
 		
-		function getTransactions(address) {
+		getTransactions(address) {
 			var url = 'https://digiexplorer.info/api/addr/'+address;
-			
+			var that = this;
 			return new Promise((resolve, reject) => {
 				checkExplorer(address);
 				function checkExplorer(address) {
-					$.getJSON( url, function( data ) {
+					$.getJSON( url,  (data) => {
 						
-						showStatus('address','Scan QR with DigiByte Payment App');
-						showStatus('transactions','Checking address for transactions...');
+						that.showStatus('address','Scan QR with DigiByte Payment App');
+						that.showStatus('transactions','Checking address for transactions...');
 						if(txcount === data.transactions.length) {
 							setTimeout(checkExplorer,5000,address);
 							
@@ -260,8 +284,8 @@
 							
 							
 							
-							showLoader();
-							showStatus('address','Transactions found on address','ready');
+							that.showLoader();
+							that.showStatus('address','Transactions found on address','ready');
 							resolve(data.transactions);
 							
 						}
@@ -275,14 +299,15 @@
 			
 		}
 	
-		function checkTransactions(txids) {
+		checkTransactions(txids) {
 			var txs = [];
+			var that = this;
 			return new Promise((resolve, reject) => {
 
 				var promises = [];
 				// loop al TX and see if the confirmations are all in check				
 				for(var i in txids) {
-					promises.push(_checkTransaction(txids[i]));
+					promises.push(_checkTransaction(txids[i],this.settings.requiredConfirmations));
 				}
 
 				Promise.all(promises)
@@ -296,7 +321,7 @@
 					
 			});
 				
-			function _checkTransaction(tx) {
+			function _checkTransaction(tx,rc) {
 					
 					var url = 'https://digiexplorer.info/api/tx/'+tx;
 					
@@ -306,11 +331,11 @@
 							$.getJSON( url, function( data ) {
 								// cast tot confirmations to an int
 								var tc = parseInt(data.confirmations) || 0;
-								if(tc >= settings.requiredConfirmations) {
+								if(tc >= rc ) {
 									txs.push(data);
 									resolve();
 								} else {
-									showStatus('transactions','Waiting for confirmations '+tc+'/'+settings.requiredConfirmations,'pending');
+									that.showStatus('transactions','Waiting for confirmations '+tc+'/'+rc+'pending');
 									// lets wait for the average blocktiming devided by 2
 									setTimeout(loopconfirm,(7500))
 								}
@@ -325,20 +350,20 @@
 			}
 		
 		}
-		function showStatus(name,message,classname) {
-			var el = main.find('.status .'+name+' .inner').html(message)
+		showStatus(name,message,classname) {
+			var el = this.main.find('.status .'+name+' .inner').html(message)
 			
 			
 			if(classname) {
 				el.addClass(classname);
 			}
 			
-			if(settings.onStatusUpdate) {
-				settings.onStatusUpdate({name,message,'flag':classname});
+			if(this.settings.onStatusUpdate) {
+				this.settings.onStatusUpdate({name,message,'flag':classname});
 			}
 		}
 		
-		function fgp(parameterName) {
+		fgp(parameterName) {
 			var result = null,
 				tmp = [];
 			var items = location.search.substr(1).split("&");
@@ -352,33 +377,33 @@
 	
 	
 	
-		function finishPayment(address) {
+		finishPayment(address) {
 			// send the total of the generated address to the selected address specified in the instatiation of the jquery payment element
 			
 			return new Promise((resolve, reject) => {
-				DGB.getWalletValue(bufferPublicAddress).then(
+				this.DGB.getWalletValue(this.bufferPublicAddress).then(
 					wval=>{
 						
 						// destinations are being made according to the amount that needs to be paid to the main address.
 						var destinations = {};
 						
 						// fee is for the mainwallet
-						destinations[settings.address] = (settings.amount);
+						destinations[this.settings.address] = (this.settings.amount);
 						
 						console.log(destinations);
 					
-						if(wval.balanceSat > settings.amount+settings.fee) {
+						if(wval.balanceSat > this.settings.amount+this.settings.fee) {
 							// spare money will be returned to the last sender.
-							destinations[address]= (wval.balanceSat-(settings.amount+settings.fee));
+							destinations[address]= (wval.balanceSat-(this.settings.amount+this.settings.fee));
 							
 						}
 						// transaction is being created with the 2 addresses
-						DGB.createTransaction(bufferPrivateKey, bufferPublicAddress, destinations,bufferPublicAddress,settings.fee,data).then(
+						this.DGB.createTransaction(this.bufferPrivateKey, this.bufferPublicAddress, destinations,this.bufferPublicAddress,this.settings.fee,this.data).then(
 							tx=>{
 								console.log(tx);
 								// send transaction, if it worked
-								DGB.sendTransaction(tx).then(result=>{
-										setStatus('endTx',result);
+								this.DGB.sendTransaction(tx).then(result=>{
+										this.setStatus('endTx',result);
 										resolve(result)
 									},
 									error=>{
@@ -414,28 +439,43 @@
 	$.fn.digipay = function (options) {
 		var settings = $.extend({}, $.fn.digipay.defaults, options);
 		// these paramters are required
+		console.log();
+		var required = [];
 		
-		var required = ['address','amount'];
 		for(var i in required) {
 			if(typeof settings[required[i]] === 'undefined') {
-				 throw 'digipay requires '+required[i]+' for its initiation';
+				 throw 'DigiPay requires '+required[i]+' for its initiation';
 			}
 		}
 
+		// is it called correctly using an ID instead of a class.
+		
+		if(this.selector.indexOf('#') === -1) {
+			throw('DigiPay can only be called with an ID, not a class or "naked" element');
+		}
+		
+		// get result of validation;
 		var res = $.fn.digipay.validate(settings)
 		
+		
+		
+		
+		
+		
 		if(res.valid) {
+			var dgp = new DigiPay(settings,$(this));
 			
-			return $(this).append(generatePaymentRequest(settings));
+			
+			$(this).append(dgp.getHtml());
+			$(this).data('_digipay',dgp);
+			return this;
 			
 		} else {
 			
 			for(var i in res.reason) {
 				throw("\n"+res.reason.join("\n\n"))
 			}
-	
-			
-			
+
 		}
 		
 		
@@ -445,7 +485,11 @@
 	};
 	
 	$.fn.digipay.validate = function(settings) {
-		console.log(bytelength(settings.data))
+	
+		
+		
+		
+		
 		var res = {
 			valid:true,
 			reason:[]
@@ -476,14 +520,42 @@
 	};
 	
 	$.fn.digipay.defaults = {
-		
+		// size of the QR, not the element. 
 		size:300,
+		
+		
+		
+		// required confirmations of the network to accept the payment as legit. i set 3, but for small payments. 
+		//it is best to do this at 6, but that takes long ( block timing is about 15 seconds )
 		requiredConfirmations : 3,
+		
+		
+		
+		// Fucntion that is fired on the end of a successful payment
 		onSuccess:false,
+		
+		
+		
+		// when the element is initialiazed, it returns a object with some properties that could be usefull.
 		onInitialize:false,
+		
+		
+		// this function is called when the transaction fails. In this case when the internet connection is lost.
 		onFail:false,
+		
+		
+		
+		// this is the fee that is used to send the tx to digiexplorer.info to make sure the payment goes throgh, As of march 2018 it is required to have a minimum of 70000 sat.
 		fee:70000,
+		
+		
+		
+		// this function will be called when the script updates is status during verification of the payment process.
 		onStatusUpdate:false,
+		
+		
+		
+		// this parameter specifies if a validation of input needs to be done. It is highly recommended to leave this on true.
 		validate:true
 	};
 	
